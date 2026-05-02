046574c05430ec4046e38d31aaf37c5593c1ea52
KudoDo221b <dovanhieu.tienphuoc@gmail.com>
2026-05-03 00:30:29 +0700

    fix: add mysql_port to config

--- config.py
+++ config.py
@@ -37,6 +37,7 @@ def get_mysql_connection():
     try:
         conn = mysql.connector.connect(
             host=os.environ.get('MYSQL_HOST'),
+            port=int(os.environ.get('MYSQL_PORT', 3306)),
             user=os.environ.get('MYSQL_USER'),
             password=os.environ.get('MYSQL_PASSWORD'),
             database=os.environ.get('MYSQL_DATABASE'),
